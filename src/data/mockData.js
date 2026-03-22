158dee1857211e250d38fffbe82f21b0d34666ff
paganid86-jpg <paganid86@gmail.com>
2026-03-22 04:40:25 +0100

    feat: add moduli.js with 15 formations; remove from mockData

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -14,7 +14,6 @@ export const trattative = [];
 
 export const svincolati = [];
 
-export const moduli = ['4-3-3', '3-4-3', '4-4-2', '3-5-2', '4-2-3-1', '5-3-2'];
 
 export const statisticheStagione = {
   puntiTotali: 0,
